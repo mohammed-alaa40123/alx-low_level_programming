1b6fcc12e2723d3179cfd8f348d1859f6a482c88
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-03-22 05:08:17 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,14 @@
 	 */
 
 
-	void print_alphabet(void)
-	{
-		char letter;
+void print_alphabet(void)
+{
+char letter;
 
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 
 
-		_putchar('\n');
-	}
+_putchar('\n');
+}
