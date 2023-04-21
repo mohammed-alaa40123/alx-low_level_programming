9af6e65f03a8f86dee041428183939ecd70e17d9
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-04-21 21:27:08 +0000

    o

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+
+#include "function_pointers.h"
+/**
+ *@name: string to print.
+ *@f: this a function pointer, that recieves a string.
+
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
+}
