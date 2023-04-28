9a3ce3f0b8f7c4e70659b345dfc2c0af906416a5
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-04-28 03:32:11 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,11 +8,11 @@
 */
 size_t list_len(const list_t *head)
 {
-	unsigned int i = 0;
-	while (head != NULL)
-	{
-		head = head->next;
-		i++;
-	}
-	return (i);
+    unsigned int i = 0;
+    while (head != NULL)
+    {
+	head = head->next;
+	i++;
+    }
+return (i);
 }
