e579e16679a3f778354f7d84b23bef96559b70dc
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-04-28 03:30:43 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * list_len - Return the number of elements in a linked list_t
- * list
- * @head: A constant double pointer to a structure of type list_t, which represents the beginning of a linked list.
- * Return: Num of nodes
- */
+* list_len - Return the number of elements in a linked list_t
+* list
+* @head: A constant double pointer to a structure of type list_t, which represents the beginning of a linked list.
+* Return: Num of nodes
+*/
 size_t list_len(const list_t *head)
 {
 	unsigned int i = 0;
