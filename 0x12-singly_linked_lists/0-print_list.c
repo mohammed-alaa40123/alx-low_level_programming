f72551d2e13f7d2f981463f82746a28522d4f088
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-04-28 03:25:48 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * print_list - Print all the elements of a list_t list
  * @head: Const  double pointer to a structure of type list_t, which represents the beginning of a linked list.
  * Return: Num of nodes
  */
-
 size_t print_list(const list_t *head)
 {
     unsigned int i = 0;
-
     while (head != NULL)
     {
         if (head->str == NULL)
             printf("[0] (nil)\n");
-
         else
             printf("[%d] %s\n", head->len, head->str);
-
         head = head->next;
         i++;
     }
