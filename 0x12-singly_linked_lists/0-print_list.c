f3ea641d9d34af81357abee79d50c22d3ad06bbf
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-04-28 03:05:45 +0200

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include "lists.h"
+
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
